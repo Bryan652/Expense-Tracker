94e543df32801ee55d12d0427727eb9e59d58d83
jhparente <parentejohnrel98@gmail.com>
2026-05-06 00:33:40 +0800

    Enable overriding of environment variables when loading .env file

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 # Load .env file
 env_path = Path(__file__).parent / '.env'
-load_dotenv(env_path)
+load_dotenv(env_path, override=True)
 
 class Settings:
     """FastAPI Settings"""
